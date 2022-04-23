3424fc230737d0c6817f230b8e3018446b88c765
Ywxig <82500877+Ywxig@users.noreply.github.com>
2022-04-23 08:35:03 +0300

    Add files via upload

---
+++ PasPogger Py/CONFIG.py
@@ -0,0 +1,36 @@
+# Настройка прогармных виджетов!
+
+#from pickle import TRUE
+
+
+#MainBG = '#c3c3c3' #Задний фон программы
+#MainTextBG = '#36393e' #Задний фон текста
+#MainTextFG = '#ffffff' #Цвет шрифта
+#Font = 'Calibri' #Шрифт
+#MainSize = '750x600' #рАЗМЕР ГЛАВНОГО ОКНА!
+#InfoSize = '690x100'
+#MainIco = 'iCO-3.ico'
+ShowTerminol = True
+ShowTerminolOnCanvas = True
+ShowButton = True
+NAME_PROGRAM = '__Main__'
+NAME_INFO_WIN = '__Info__'
+
+# Настройка подсветки синтоксиса
+
+KeyWord = '#35dd13'
+LogikKeyWord = '#f8df00'
+Sigle = '#35dd13'
+Double = '#c4ff0e'
+VarLight = '#35dd13'
+
+
+
+GenCase = '#50c5ff'# Настройка спец символов {!, !r, !d}
+GenHtml = '#ae03ff'# Настройка спец символов { @, @s, @p}
+
+
+# Настройка терминалла!
+TerminalIco = 'ico-2.ico'
+TerminaTextBG = '#43464d' #Задний фон текста
+TerminaTextFG = '#ffffff' #Цвет шрифта
